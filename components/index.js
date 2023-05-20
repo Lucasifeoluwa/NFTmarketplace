fb459af007fac22ce83944d7e56a8c7ce6fa4702
lucasifeoluwa <lucasifeoluwa@yahoo.com>
2023-05-20 13:18:47 +0100

    NFT Maeketpace

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import NewCard from "./NewCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+
+export {
+  HomeHeader,
+  NewCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  DetailsDesc,
+  DetailsBid,
+  SubInfo,
+};
